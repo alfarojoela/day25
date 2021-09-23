83cf9d9d772e95b1a452b9c2b7969e301a6bcc5c
Joel Alfaro <alfarojoela@outlook.com>
2021-09-23 12:39:11 -0700

    better organization of comments

--- main.py
+++ main.py
@@ -1,45 +1,14 @@
 import csv
 import pandas
 
-#other format:
-#   data_file = open("weather_data.csv")
-#   data = data_file.readlines()
-#   data_file.close()
-
-
-
-# with open("weather_data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != "temp":
-#             temperatures.append(int(row[1]))
-#
-# print(temperatures)
-
 data = pandas.read_csv("weather_data.csv")
-data_dict = data.to_dict()
-temp_list = data["temp"].to_list()
+print(type(data))  #this is a DataFrame object from pandas library equivalent to a whole spreadsheet table.
+                    #has many methods that allow conversion to specific data files like html, json, etc.
 
-average = sum(temp_list) / len(temp_list)
+print(type(data["temp"])) #panda library Series object very similiar to a single column in a table.  behaves like a
+                            #list datatype
 
-print(data["temp"].mean())
-
-print(data["temp"].max())
-
-#get data in columns
-print(data["condition"])
-print(data.condition)
-
-#get data in row
-#data[data["day"]]
-
-data[data.day == "Monday"]
-
-print(data[data.temp == data.temp.max()])
-
-monday = data[data.day == "Monday"]
-print(monday.condition)
+print(data["temp"])
 
 
 
